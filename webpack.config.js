15a97f74d5a711a583f3ac451ab5386bbb9ea81f
伯野祥展 <yoshihirohakuno@hakunoyoshihiroMacBook-Pro.local>
2021-09-18 17:56:16 +0900

    Generate HTML

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,22 @@ module.exports = {
                         name: "images/[name].[ext]",
                     },
                 }
-            ]
-            }
+             ]
+            },
+            {
+                test: /\.pug/,
+                use:[
+                    {
+                        loader: "html-loader",
+                        options: {
+                            pretty: true,
+                        }
+                    },
+                    {
+                        loader: "pug-html-loader",
+                    },
+                ],
+            },
         ],
     },
     plugins: [
@@ -41,7 +55,12 @@ module.exports = {
             filename:"./stylesheets/main.css",
         }),
         new HtmlWebpackPlugin({
-            template: "./src/templates/index.html",
+            template: "./src/templates/index.pug",
+            filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/templates/access.pug",
+            filename: "access.html",
         }),
         new CleanWebpackPlugin(),
     ],
